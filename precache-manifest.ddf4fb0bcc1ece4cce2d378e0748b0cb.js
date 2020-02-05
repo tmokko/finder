e154a13b43b5c47616eed8ce86d280bcfbdab09c
tmokko <58773830+tmokko@users.noreply.github.com>
2020-02-05 11:16:52 +0200

    Updates

---
+++ precache-manifest.ddf4fb0bcc1ece4cce2d378e0748b0cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb503d26f9855924ecb86794c96d311",
+    "url": "/finder/index.html"
+  },
+  {
+    "revision": "a4b9949f2be797721349",
+    "url": "/finder/static/css/main.7eb43b69.chunk.css"
+  },
+  {
+    "revision": "374230321e0d05e0de37",
+    "url": "/finder/static/js/2.917a5894.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/finder/static/js/2.917a5894.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4b9949f2be797721349",
+    "url": "/finder/static/js/main.0c4af3dc.chunk.js"
+  },
+  {
+    "revision": "784144b52f46937fe454",
+    "url": "/finder/static/js/runtime-main.b174c9fc.js"
+  },
+  {
+    "revision": "75ae2ba25d3d06d5079a940b79114940",
+    "url": "/finder/static/media/spinner.75ae2ba2.gif"
+  }
+]);
